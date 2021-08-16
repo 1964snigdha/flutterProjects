500bbd9015eb255bb864f5d39cf2fc74375af9c5
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-16 14:41:25 +0530

    updatedTitleAndSettingsIconInBottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ void main()
   runApp(MaterialApp(
     home: Scaffold(
     appBar: AppBar(
-      title: const Text('Updated Landing Page'),backgroundColor:Colors.deepOrange[400],centerTitle: true, 
+      title: const Text('Updated2.0 Landing Page'),backgroundColor:Colors.deepOrange[400],centerTitle: true, 
     ),
     floatingActionButton: FloatingActionButton(onPressed: null,child: Text("click"),
     ),
     bottomNavigationBar: BottomNavigationBar(items: [
       BottomNavigationBarItem(icon: Icon(Icons.home),title: Text('Home'),),
-      BottomNavigationBarItem(icon: Icon(Icons.backpack),title: Text('Backpack'),),
+      BottomNavigationBarItem(icon: Icon(Icons.settings),title: Text('Settings'),),
     ]),
     ),
   ),
