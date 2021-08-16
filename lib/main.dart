4eb306ceffb2949464f8e54bb649f719f4b9c7db
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-16 11:26:33 +0530

    Exploring scaffold class and adding bottom navigation property

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ void main()
     appBar: AppBar(
       title: const Text('Welcome to Dewdas Technology'),backgroundColor:Colors.deepOrange[400],centerTitle: true, 
     ),
+    floatingActionButton: FloatingActionButton(onPressed: null,child: Text("click"),
+    ),),
   ),
  
-)
   );
 }
 
