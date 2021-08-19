0ad9673ab6c9d4d95e0f17b725b86887883bb85b
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-19 13:18:40 +0530

    testCase3:ExploringRowWidgetProperty

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,20 @@ void main(){
          backgroundColor: Colors.red[600],
          centerTitle: true,
        ),
-       body: Center(
-         child: 
-         Container(
-         child:new Text('Launching my First Application',style: TextStyle(fontSize: 40),
-         ) ,
-         color: Colors.grey[800],
-         decoration: BoxDecoration(
-           gradient: LinearGradient(color: [
-             Colors.lightGreen,Colors.red,
-           ])
-         ),
-         ),
-         ),
+       body: Row(
+            children: [
+              Text('Hello World'),
+              FlatButton(onPressed: () {},
+              color: Colors.amber,
+              child: Text('click me'),
+              ),
+              Container(
+                color: Colors.cyan,
+                padding:EdgeInsets.all(30.0),
+                child: Text('Inside Container'),
+              ),
+            ],
+       ),
      ),
    ));
 }
