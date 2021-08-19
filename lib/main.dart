c45f2b4704eac22079b2af46d997bdf7b26aa3e1
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-19 11:52:57 +0530

    testCase1:ExploringContainerWidgetsAnditsProperties

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-void main()
-{
-runApp(new MaterialApp(
-home:new Test(), 
-)
-);
-}
-
-class Test extends StatelessWidget {
-  const Test({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-  appBar: AppBar(
-    title: new Text('My first Flutter Application'),
-  ),
-  body: new Center(
-child: new Text('Hello Subhadip!!!!'),
-  ),
-  floatingActionButton: new FloatingActionButton(onPressed: () {},child: Text('Click'),
-  ),
-  );
-      
-  }
+void main(){
+   runApp(MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Scaffold(
+       appBar: AppBar(
+         title: new Text('My First application'),
+         backgroundColor: Colors.red[600],
+         centerTitle: true,
+       ),
+       body: //Center(
+         //child: 
+         Container(
+         child:new Text('Launching my First Application',style: TextStyle(fontSize: 40),
+         ) ,
+         color: Colors.grey[800],
+         height: 200,
+         width: 400,
+         alignment: Alignment.center,
+         padding: EdgeInsets.all(40.0),
+         margin: EdgeInsets.all(30.0),
+         ),
+         ),
+     ),
+   );
 }
 
 
