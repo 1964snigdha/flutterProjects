b1168f4aba888fa702ec5a7de749e8107a7a1aa7
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-19 12:19:55 +0530

    testCase2error:ExploringGradientProperty

--- lib/main.dart
+++ lib/main.dart
@@ -10,21 +10,21 @@ void main(){
          backgroundColor: Colors.red[600],
          centerTitle: true,
        ),
-       body: //Center(
-         //child: 
+       body: Center(
+         child: 
          Container(
          child:new Text('Launching my First Application',style: TextStyle(fontSize: 40),
          ) ,
          color: Colors.grey[800],
-         height: 200,
-         width: 400,
-         alignment: Alignment.center,
-         padding: EdgeInsets.all(40.0),
-         margin: EdgeInsets.all(30.0),
+         decoration: BoxDecoration(
+           gradient: LinearGradient(color: [
+             Colors.lightGreen,Colors.red,
+           ])
+         ),
          ),
          ),
      ),
-   );
+   ));
 }
 
 
