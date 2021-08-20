c2a0d8b92c8384ba0599ee2e37bb1f8eb79feaa7
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-20 13:09:48 +0530

    testCase1:TextWidgetProperty

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,26 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-void main(){
-   runApp(MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-       appBar: AppBar(
-         title: new Text('My First application'),
-         backgroundColor: Colors.red[600],
-         centerTitle: true,
-       ),
-       body: Row(
-            children: [
-              Text('Hello World'),
-              FlatButton(onPressed: () {},
-              color: Colors.amber,
-              child: Text('click me'),
-              ),
-              Container(
-                color: Colors.cyan,
-                padding:EdgeInsets.all(30.0),
-                child: Text('Inside Container'),
-              ),
-            ],
-       ),
-     ),
-   ));
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new Home(),
+  ));
 }
 
-
-
-
-
- 
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text('Flutter Text Widget'),
+      ),
+      body:  new Text(
+        'Lorem Ipsum Is Simply text of Printing.Lorem Ipsum is simply dummy text of the printing',
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center,
+        //overflow: TextOverflow.ellipsis,
+        textScaleFactor:2.0  ,
+      ),
+    );
+  }
+}
