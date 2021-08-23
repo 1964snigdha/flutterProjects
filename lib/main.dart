1acac4c7e1fbae19b6bd1b3d4fc2b57e76ee7317
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-23 11:59:47 +0530

    testCase3:ExploringMorePropertyonTextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: new Home(),
+    title:('Text Exploring Text Widget Class'),
+    home: new MyHome(),
   ));
 }
 
-class Home extends StatelessWidget {
+class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,19 +16,24 @@ class Home extends StatelessWidget {
         title: new Text('Flutter Text Widget'),
       ),
       body: new Text(
-        'Lorem Ipsum Is Simply text of Printing.Lorem Ipsum is simply dummy text of the printing',
+        'The virus that causes COVID-19 is in a family of viruses called Coronaviridae. Antibiotics do not work against viruses.Some people who become ill with COVID-19 can also develop a bacterial infection as a complication. In this case, antibiotics may be recommended by a health care provider.There is currently no licensed medication to cure COVID-19. If you have symptoms, call your health care provider or COVID-19 hotline for assistance.',
         textDirection: TextDirection.ltr,
-        textAlign: TextAlign.right,
-        //overflow: TextOverflow.ellipsis,
-        //textScaleFactor:2.0  ,
+        textAlign: TextAlign.center,
+        overflow: TextOverflow.fade,
+       // textScaleFactor:1.5  ,
+       //maxLines:2 it will show the maximum number of lines to show ,
         style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
+          fontSize: 30.0,
+          fontWeight: FontWeight.w400,
           wordSpacing: 20.0,
-          letterSpacing: 5.0,
-          color: Colors.redAccent,
-          backgroundColor: Colors.greenAccent,
-          fontStyle: FontStyle.italic,
+          letterSpacing: 2.0,
+          //color: Colors.pink[600],
+          foreground: Paint()
+          ..color=Colors.yellowAccent,
+          background: Paint()
+          ..color=Colors.cyanAccent,
+          //backgroundColor: Colors.deepOrange[100],
+          fontStyle: FontStyle.normal,
           decoration: TextDecoration.underline,
         ),
       ),
