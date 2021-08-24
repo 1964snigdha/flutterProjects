8f0c29c0f8b16ccf6a893f74586addf41f66c679
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-24 12:10:13 +0530

    testCase1:ExploringTextFieldClassForImplementingForms

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,26 @@ class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: new Text('Flutter Text Widget'),
-        ),
-        body:Text.rich(
-          TextSpan(
-            text: 'Hello',
-            style: TextStyle(fontSize: 20.0,color: Colors.cyan),
-            children: <InlineSpan>[
-              TextSpan(
-                text: 'World',
-                style: TextStyle(fontSize: 30.0,color: Colors.orange),
-                
-              ),
-             TextSpan(
-                text: 'Subhadip',
-                style: TextStyle(fontSize: 40.0,color: Colors.purple),
-                
-              ), 
-            ]
-
-        )));
+      appBar: AppBar(
+        title: new Text('Flutter Text Widget'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+                labelText: "Name",
+                hintText: "Your Name",
+                labelStyle: TextStyle(fontSize: 20.0, color: Colors.black),
+                border: OutlineInputBorder(),
+                prefixIcon:Icon(Icons.security) 
+                ),
+                keyboardType: TextInputType.number,
+                //obscureText: true,
+                maxLength: 10,
+                maxLines: 4,
+          )
+        ],
+      ),
+    );
   }
 }
