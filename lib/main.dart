1c8f9ba04bb8e4bdcbacb5cb1ca5084019efb662
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-28 20:29:01 +0530

    testCase1:ExploringListTileClass

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Retrieve Text Input',
-      home: MyCustomForm(),
-    );
-  }
-}
-
-// Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({Key? key}) : super(key: key);
-
-  @override
-  _MyCustomFormState createState() => _MyCustomFormState();
-}
-
-// Define a corresponding State class.
-// This class holds the data related to the Form.
-class _MyCustomFormState extends State<MyCustomForm> {
-  // Create a text controller and use it to retrieve the current value
-  // of the TextField.
-  final myController = TextEditingController();
-
-  @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    myController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Retrieve Text Input'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // When the user presses the button, show an alert dialog containing
-        // the text that the user has entered into the text field.
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                // Retrieve the text the that user has entered by using the
-                // TextEditingController.
-                content: Text(myController.text),
-              );
-            },
-          );
-        },
-        tooltip: 'Show me the value!',
-        child: const Icon(Icons.text_fields),
-      ),
-    );
+        body:Center(
+          child: Text('Drawer Applicaton'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.all(0.0),
+            children: [
+              UserAccountsDrawerHeader(
+                accountEmail: Text("1964Snigdha@gmail.com"),
+                accountName: Text("Subhadip Mondal"),
+                currentAccountPicture: CircleAvatar(
+                  child: Text('MyPIC'),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.verified_user),
+                title: Text('Subhadip First Application'),
+                subtitle: Text('Manoj Sir'),
+                trailing: Icon(Icons.delete),
+                onTap: () {},
+
+              ),
+               ListTile(
+                leading: Icon(Icons.verified_user),
+                title: Text('Subhadip First Application'),
+                subtitle: Text('Manoj Sir'),
+                trailing: Icon(Icons.delete),
+                onTap: () {},
+                onLongPress: () {
+                  Text("LOng Press");
+                },
+
+              ),
+            ],
+
+          ),
+        ),
+        )
+          
+        );
+        
+    
+    
   }
 }
