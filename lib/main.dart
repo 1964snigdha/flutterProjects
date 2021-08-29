cc1896816ce4fe93527fc59f9e78a7107a652803
1964snigdha <88871878+1964snigdha@users.noreply.github.com>
2021-08-29 19:58:09 +0530

    testCase1:ExploringExpandedClass

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Expanded Widget',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Expanded Widget'),
         ),
-        body: Row(
-          children: [
-             const FlutterLogo(),
-    const Expanded(
-      child: Text("Flutter's hot reload helps you quickly and easily experiment, build UIs, add features, and fix bug faster. Experience sub-second reload times, without losing state, on emulators, simulators, and hardware for iOS and Android."),
-    ),
-    const Icon(Icons.sentiment_very_satisfied),
-  ],
+        body:Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.amber,
+                width: 100,
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              height: 100,
+              width: 100,
+            ),
+          ],
         ),
-      )
-          
-          
-        );
+      ),
+    ),);
   }
-}
-        
+} 
